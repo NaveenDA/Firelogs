dced6c1e9e97152c3eb5279fb7ab83c3bed4a4d3
Naveen DA <mailtomassnaveen@gmail.com>
2021-06-10 00:59:55 +0530

    Initial commit

---
+++ background.js
@@ -0,0 +1,17 @@
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({
+    file: "./dist/bundle.js"
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+});
---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+
+var div=document.createElement("div"); 
+document.body.appendChild(div); 
+div.innerText="test123";
