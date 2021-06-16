0e2e482c162530b42cc91400dc0de7e8fc786290
Naveen DA <mailtomassnaveen@gmail.com>
2021-06-17 02:53:30 +0530

    Add chrome and firefox builds

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,18 @@
 var fs = require("fs-extra");
 
-fs.mkdirp("chrome");
-fs.emptyDirSync("chrome");
-fs.copy("./dist", "./chrome/dist");
-fs.copy("./background.js", "./chrome/background.js");
-fs.copy("./manifest.json", "./chrome/manifest.json");
+fs.emptyDirSync("out");
+/**
+ * Chrome Buid
+ */
+fs.copySync("./dist", "./out/chrome/dist");
+fs.copySync("./dist/background.js", "./out/chrome/background.js");
+fs.removeSync("./out/chrome/dist/background.js");
+fs.copySync("./manifest.json", "./out/chrome/manifest.json");
+
+/**
+ * Firefox Build
+ */
+fs.copySync("./dist", "./out/firefox/dist");
+fs.copySync("./dist/background.js", "./out/firefox/background.js");
+fs.removeSync("./out/firefox/dist/background.js");
+fs.copySync("./manifest.json", "./out/firefox/manifest.json");
