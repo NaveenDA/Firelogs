f3ace7177a6fddcb538a8146bb6745a272104056
Naveen <mailtomassnaveen@gmail.com>
2021-06-20 12:25:09 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,49 @@
 const path = require("path");
+const TerserPlugin = require('terser-webpack-plugin');
 
 
 module.exports = {
-  mode: process.env.NODE_ENV !== "production" ? "development" : "production",
-  entry: {
-    "dist/firelogs": path.resolve(path.join(__dirname, "./src/firelogs/index.js")),
-    "dist/firelogs-tabs": path.resolve(__dirname, "./src/tabs/index.js"),
-    "dist/background": path.resolve(__dirname, "./src/extension/index.js"),
-    "dist/content": path.resolve(__dirname, "./src/response/index.js"),
-  },
-  output: {
-    path: path.resolve(__dirname),
-    filename: "[name].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader"
+
+    mode: process.env.NODE_ENV !== "production" ? "development" : "production",
+    entry: {
+        "dist/firelogs": path.resolve(path.join(__dirname, "./src/firelogs/index.js")),
+        "dist/firelogs-tabs": path.resolve(__dirname, "./src/tabs/index.js"),
+        "dist/background": path.resolve(__dirname, "./src/extension/index.js"),
+        "dist/content": path.resolve(__dirname, "./src/response/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname),
+        filename: "[name].js"
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            extractComments: false,
+        })],
+    },
+    module: {
+        rules: [{
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: "url-loader",
+                options: {
+                    limit: true
+                }
+            },
+            {
+                test: /\.hbs$/,
+                loader: "handlebars-loader"
+            }
         ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: "url-loader",
-        options: {
-          limit: true
-        }
-      },
-      {
-        test: /\.hbs$/,
-        loader: "handlebars-loader"
-      }
-    ]
-  }
+    },
+
 };
