0051945c49a565f18f4aad76870f42af9daf73e9
Naveen <mailtomassnaveen@gmail.com>
2021-06-20 12:26:09 +0530

    Update prestart.js

--- scripts/prestart.js
+++ scripts/prestart.js
@@ -1,3 +1,5 @@
 var fs = require("fs-extra");
+
 fs.emptyDirSync("dist");
-fs.copyFileSync("./src/firelogs.html","./dist/firelogs.html");
+fs.copyFileSync("./src/firelogs.html", "./dist/firelogs.html");
+fs.copyFileSync("./manifest.json", "./dist/manifest.json");
