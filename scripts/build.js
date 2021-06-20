d44207a8de8746f1ef1bb097f04c2a591e2d33c6
Naveen <mailtomassnaveen@gmail.com>
2021-06-20 12:25:45 +0530

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -1,18 +1,23 @@
 var fs = require("fs-extra");
-
+var buildNumber = process.env.npm_package_version
 fs.emptyDirSync("out");
+/**
+ * Update build number to the Manifest JSON
+ */
+var manifest = fs.readFileSync("./manifest.json").toString();
+manifest = JSON.parse(manifest);
+manifest.version = buildNumber;
+fs.writeFileSync("./manifest.json", JSON.stringify(manifest, null, 2));
 /**
  * Chrome Buid
  */
 fs.copySync("./dist", "./out/chrome/dist");
-fs.copySync("./dist/background.js", "./out/chrome/background.js");
-fs.removeSync("./out/chrome/dist/background.js");
 fs.copySync("./manifest.json", "./out/chrome/manifest.json");
+fs.removeSync("./out/chrome/dist/manifest.json");
 
 /**
  * Firefox Build
  */
 fs.copySync("./dist", "./out/firefox/dist");
-fs.copySync("./dist/background.js", "./out/firefox/background.js");
-fs.removeSync("./out/firefox/dist/background.js");
+fs.removeSync("./out/firefox/dist/manifest.json");
 fs.copySync("./manifest.json", "./out/firefox/manifest.json");
